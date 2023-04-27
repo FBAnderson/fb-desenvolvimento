95f4e0240552016e3839006926fbacea1374e74c
Anderson Bastos <fbanderson.suporte@outlook.com.br>
2023-04-27 19:26:03 -0300

    Trabalhando na tela de pesquisa

--- site/pages/ficha-ativacao.php
+++ site/pages/ficha-ativacao.php
@@ -10,9 +10,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/layout/ficha-ativacao.css">
+    <link rel="stylesheet" href="../css/menus/barra-menus.css">
     <title>Ficha de Ativação</title>
 </head>
 <body>
-    
+    <div>
+        <header class="cabecalho">
+                        <a class="link-menu" href="../../../index.php"><img src="../img/icons/icon-home.png" title="Clique para voltar"><p class="texto">Fichas de Ativação</p></a></li>
+        </header>
+        <div class="painel-pesquisa">
+
+        </div>
+    </div>
 </body>
 </html>
