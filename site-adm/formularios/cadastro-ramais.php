ea69062adf03968536f589554330951881cab1d1
Anderson Bastos <anderson.bastos@krona.net>
2023-04-29 16:22:55 -0300

    trabalhando com a tela de cadastro dos ramais

--- site-adm/formularios/cadastro-ramais.php
+++ site-adm/formularios/cadastro-ramais.php
@@ -13,25 +13,19 @@
     <h1 class="titulo-formulario">Cadastro de Ramais</h1>
     <form method="post" action="#" class="form-control">
         <label class="rotulo-campo">Ramal:&nbsp;</label>
-        <input name="ramal" type="text" class="dado-campo-r" placeholder="" maxlength="" autocomplete="off" required>
+        <input name="ramal" type="text" class="dado-campo-r" placeholder="" maxlength="6" autocomplete="off" required>
         <label class="rotulo-campo">Setor:&nbsp;&nbsp;&nbsp;</label>
         <select class="dado-campo" placeholder="" maxlength="" autocomplete="off" required>
             <option value="Compartilhada" class="dado-campo">Central Compartilhada</option>
-            <option value="Bsoft" class="dado-campo">Bsoft</option>
-            <option value="A3" class="dado-campo">A3Software</option>
-            <option value="DSS" class="dado-campo">DSS</option>
-            <option value="Datapar" class="dado-campo">Datapar</option>
-            <option value="ESL" class="dado-campo">ESL</option>
-            <option value="Escala-soft" class="dado-campo">Escala Soft</option>
-            <option value="Rodopar" class="dado-campo">Rodopar</option>
-            <option value="SistGlobal" class="dado-campo">SistGlobal</option>
-            <option value="MultiSotware" class="dado-campo">Multisoftware</option>
-            <option value="AtenaInformatica" class="dado-campo">Atena Informática</option>
-            <option value="O2" class="dado-campo">O<sub>2</sub></option>
-            <option value="Alisson" class="dado-campo">Alisson</option>
-            <option value="Brudam" class="dado-campo">Brudam</option>
-            <option value="Software-proprietario" class="dado-campo">Software-proprietario</option>
-            <option value="Atena" class="dado-campo">Atena</option>
+            <option value="Pamsat-um" class="dado-campo">Central Pamsat I</option>
+            <option value="Pamsat-dois" class="dado-campo">Central Pamsat II</option>
+            <option value="Krona-Oeste" class="dado-campo">Central Krona Oeste</option>
+            <option value="Administrativo" class="dado-campo">ADM / RH / DP / Financeiro</option>
+            <option value="Diretoria" class="dado-campo">Diretoria</option>
+            <option value="cto" class="dado-campo">CTO</option>
+            <option value="Projetos" class="dado-campo">Setor de Projetos</option>
+            <option value="Qualidade" class="dado-campo">Setor de Qualidade</option>
+            <option value="pma" class="dado-campo">Central PMA</option>
         </select><br>        
         <label class="rotulo-campo">Descrição:</label><br>
         <textarea name="historico" class="historico" rows="9" cols="80"></textarea><br>
